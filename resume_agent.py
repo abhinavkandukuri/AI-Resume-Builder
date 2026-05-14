c8208dd3748d83f1a4273cdb0674f04fef7df3c9
KANDUKURI ABHINAV <abhinavkandukuri05@gmail.com>
2026-05-14 13:56:26 +0530

    Update resume_agent.py

--- resume_agent.py
+++ resume_agent.py
@@ -1,12 +1,9 @@
 import google.generativeai as genai
-from dotenv import load_dotenv
-import os
+import streamlit as st
 
-load_dotenv()
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def generate_resume(prompt):
     response = model.generate_content(prompt)
